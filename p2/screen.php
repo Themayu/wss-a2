b612dca09ed8bda8d413323c04ddf998b55fa228
Jamie Ridding <Themayu@users.noreply.github.com>
2017-11-09 11:36:37 +0000

    Add P2

---
+++ p2/screen.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Screen</title>
+  </head>
+  <body>
+    <?php
+      $width = $_POST["width"];
+      $height = $_POST["height"];
+      $browser = $_SERVER["HTTP_USER_AGENT"];
+    ?>
+
+    <h3><?php
+      echo" Your browser is: $browser";
+    ?></h3>
+
+    <h3><?php
+      echo "You are using a screen resolution of $width x $height";
+    ?></h3>
+  </body>
+</html>
