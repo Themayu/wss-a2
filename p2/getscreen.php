91a9935240f0ea9309211efb29f2bb85316b712f
Jamie Ridding <Themayu@users.noreply.github.com>
2017-11-23 10:26:19 +0000

    Move HTML head and header into new file, rename to .php

---
+++ p2/getscreen.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+      $title = "User Agent and Screen Resolution";
+      require($_SERVER["DOCUMENT_ROOT"] + "/page-struct/html-head.php");
+    ?>
+  </head>
+  <body>
+    <!-- header -->
+    <header>
+      <?php require($_SERVER["DOCUMENT_ROOT"] . "/page-struct/header.php"); ?>
+    </header>
+    <!-- /header -->
+
+    <!-- content -->
+    <main class="container">
+      <h4>Check your user agent and screen resolution...</h4>
+      <p><a href="screen.html" class="waves-effect waves-light btn">Click here for details...</a></p>
+    </main>
+    <!-- /content -->
+  </body>
+</html>
