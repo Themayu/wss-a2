86281ea0f814acb414f2d8403f850c83cec07c61
Jamie Ridding <Themayu@users.noreply.github.com>
2017-11-23 10:27:45 +0000

    Remove HTML head

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php $title = ""; ?>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <?php
+      $title = "";
+      require($_SERVER["DOCUMENT_ROOT"] + "/page-struct/html-head.php");
+    ?>
   </head>
 
   <body>
