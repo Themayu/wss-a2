4759045567bde605ef58bf5abc04dc434b28e56f
Jamie Ridding <Themayu@users.noreply.github.com>
2017-11-23 10:28:52 +0000

    P2: Point URL to ./p2/getscreen.php

--- page-struct/navigation.php
+++ page-struct/navigation.php
@@ -1,5 +1,5 @@
 <li><a href="https://github-college.github.io/web-server-scripting-Themayu/" target="_blank">Assignment 1</a></li>
-<li><a href="./p2/getscreen.html" target="_blank">P2</a></li>
+<li><a href="./p2/getscreen.php" target="_blank">P2</a></li>
 <li><a href="./p3/upload_03.html" target="_blank">P3</a></li>
 <li><a href="./p4/login.php" target="_blank">P4</a></li>
 <li><a href="./p5/guestbook.html" target="_blank">P5</a></li>
