ce46199ec943252abb4b1df1e73633d24b273fd6
Jamie Ridding <Themayu@users.noreply.github.com>
2017-11-23 10:16:25 +0000

    Move header + navigation to own files

---
+++ page-struct/header.php
@@ -0,0 +1,18 @@
+<nav>
+  <div class="nav-wrapper hide-on-large-only">
+    <a href="#" class="brand-logo">Jamie Ridding</a>
+    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+
+    <ul id="nav-mobile" class="side-nav">
+      <?php require($_SERVER["DOCUMENT_ROOT"] . "/page-struct/navigation.php"); ?>
+    </ul>
+  </div>
+
+  <div class="nav-wrapper container hide-on-med-and-down">
+    <a href="#" class="brand-logo">Jamie Ridding</a>
+
+    <ul class="right">
+      <?php require($_SERVER["DOCUMENT_ROOT"] . "/page-struct/navigation.php"); ?>
+    </ul>
+  </div>
+</nav>
---
+++ page-struct/navigation.php
@@ -0,0 +1,8 @@
+<li><a href="https://github-college.github.io/web-server-scripting-Themayu/" target="_blank">Assignment 1</a></li>
+<li><a href="./p2/getscreen.html" target="_blank">P2</a></li>
+<li><a href="./p3/upload_03.html" target="_blank">P3</a></li>
+<li><a href="./p4/login.php" target="_blank">P4</a></li>
+<li><a href="./p5/guestbook.html" target="_blank">P5</a></li>
+<li><a href="./m2/create_contact.html" target="_blank">M2</a></li>
+<li><a href="./m3/login.php" target="_blank">M3</a></li>
+<li><a href="./m4/error_4.php" target="_blank">M4</a></li>
